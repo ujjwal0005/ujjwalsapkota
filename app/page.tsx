4df8b889de47b35f3a6456c1867f467e637147bc
ujjwalsapkota005 <ujjwalsapkota005@gmail.com>
2024-08-18 22:11:23 +0545

    hero page in making

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
+import hero from "@/components/hero";
 
 export default function Home() {
   return (
- <main className="relative bg-black">
-  <div>
-    <h1>Hello, Ujjwal Sapkota</h1>
+ <main className="relative bg-black flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+  <div className="max-w-7xl w-full">
+    <div>
+      <hero />
+    </div>
   </div>
  </main>
   );
---
+++ components/hero.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const hero = () => {
+  return (
+    <div className='pb-20 pt-36'>
+      <div>
+        kjfejkfsdkjf
+      </div>
+    </div>
+  )
+}
+
+export default hero
